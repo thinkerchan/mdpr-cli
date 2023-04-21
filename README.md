0c666de2e925335cc1793f5842d4b77dea4af093
kai <chanthinker@foxmail.com>
2023-04-21 10:18:40 +0800

    bin

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+## why
 md文件中的图片格式如下:
 ```
 ![1](https://p.ipic.vip/67o2jq.jpg)
@@ -5,7 +6,18 @@ md文件中的图片格式如下:
 
 如果使用了类似iPic这样的工具上传到微博图床, 微博会因为referer的拦截而屏蔽对应图片, 用这个脚本可以下载md里面的所有符合以上格式的图片,并上传到自己的七牛云
 
-``` js
-npm install
-node index
+
+## 准备
+创建.env文件
+```
+ACCESS_KEY=
+SECRET_KEY=
+BUCKET=
+URL=
+```
+
+## 使用
+
+``` zsh
+mdpicreplace file.md
 ```
--- index.js
+++ index.js
@@ -2,9 +2,35 @@ const fs = require('fs');
 const path = require('path');
 const axios = require('axios');
 const qiniu = require('qiniu');
+const argv = require('minimist')(process.argv.slice(2))
+
+const {
+  ACCESS_KEY,
+  SECRET_KEY,
+  BUCKET,
+  URL
+} = process.env;
+
+if (!(ACCESS_KEY && SECRET_KEY &&BUCKET&& URL)) {
+  console.log('请检查环境变量是否正确')
+  return;
+}
+
+let fileName = argv._[0]
+if (!(argv._.length  && fileName.indexOf('.md')>-1)) {
+  console.log('检查文件路径是否正确!')
+  return
+}
 
 // 读取Markdown文件内容
-const mdPath = './md/1.md';
+const mdPath = process.cwd()+'/'+fileName;
+
+
+if (!fs.existsSync(mdPath)) {
+  console.log(`文件: ${fileName} 不存在!`);
+  return;
+}
+
 let mdContent = fs.readFileSync(mdPath, 'utf-8');
 
 // 匹配Markdown中的图片链接
@@ -20,10 +46,10 @@ imgUrls = imgUrls.map((imgUrl) => {
 
 console.log(`imgUrls`, imgUrls);
 
-const accessKey = ''; // 填写你的Access Key
-const secretKey = ''; // 填写你的Secret Key
-const bucket = ''
-const urlPrefix = ''; // 填写你的外链域名
+const accessKey =  ACCESS_KEY
+const secretKey =  SECRET_KEY
+const bucket =  BUCKET
+const urlPrefix = URL
 
 const mac = new qiniu.auth.digest.Mac(accessKey, secretKey);
 const options = {
@@ -34,6 +60,13 @@ const uploadToken = putPolicy.uploadToken(mac);
 const config = new qiniu.conf.Config();
 const formUploader = new qiniu.form_up.FormUploader(config);
 const putExtra = new qiniu.form_up.PutExtra();
+const maxTime = 5000;
+
+
+const tempDir = './cacheImgs';
+if (!fs.existsSync(tempDir)) {
+  fs.mkdirSync(tempDir);
+}
 
 
 function start() {
